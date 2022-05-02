0b2a1132819e6fec8e49cb1e94281915fe0abf2e
Getasew <amsalugetasew12@gmail.com>
2022-05-02 04:43:32 -0700

    after firebase database login

--- admin-dashboard/src/App.jsx
+++ admin-dashboard/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Home from './Pages/Home/Home'
 import New from './Pages/New/New'
 import Login from './Pages/Login/Login'
@@ -8,18 +8,25 @@ import "./Style/Dark.scss"
 import {useContext} from "react"
 import { productInputs, userInputs } from "./formSource";
 import { DarkModeContext } from "./Context/DarkModeContext";
+import { AuthContext } from "./Context/AuthContext";
 function App() {
-  // {dark ? "app dark:": "app"}
   const {darkMode} = useContext(DarkModeContext)
-  const {greenMode} = useContext(DarkModeContext)
-//  const [dark, setDark] = useState(false);
+  const {currentUser} = useContext(AuthContext);
+  const RequireAuth = ({children}) =>{
+    return currentUser ? children: <Navigate to="/login"/>;
+  };
+  console.log(currentUser);
   return (
-    <div className={darkMode ? "app dark": "app"} id={greenMode ? "app green": "app"}>
+    <div className={darkMode ? "app dark": "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
+            <Route index element={
+            <RequireAuth>
+              <Home />
+              </RequireAuth>
+          }/>            
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
--- admin-dashboard/src/Pages/Login/Login.jsx
+++ admin-dashboard/src/Pages/Login/Login.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {useNavigate} from "react-router-dom"
 import "./Login.scss"
 import { signInWithEmailAndPassword  } from "firebase/auth";
 import {auth} from "../../firebase"
+import {AuthContext} from "../../Context/AuthContext"
 const Login = () => {
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const {dispatch} = useContext(AuthContext)
   const handleLogin = (e) => {
     e.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
+        dispatch({type: "LOGIN", payload:user})
         navigate("/");
       })
       .catch((error) => {
